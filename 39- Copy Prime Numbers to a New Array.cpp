c66ec1df7c629ec100381859a97eda6af63b63c4
Tarek Elgohry <telgohry@gmail.com>
2022-07-31 14:43:17 +0200

    Add files via upload

---
+++ 39- Copy Prime Numbers to a New Array.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+int ReadPositiveNumber(std::string msg) {
+	int number; 
+
+	do {
+		std::cout << msg;
+		std::cin >> number;
+	} while (number <= 0);
+
+	return number;
+}
+
+bool IsPrime(int number) {
+	if (number <= 1) {
+		return 0;
+	}
+	else {
+		int m = number / 2;
+		for (int i = 2; i <= m; i++) {
+			if (number % i == 0) {
+				return 0;
+			}
+		}
+	}
+	return 1; 
+}
+
+int RandomInRange(int from, int to) {
+	int random_number = rand() % (to - from + 1) + from;
+	return random_number;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength) {
+	//std::cout << "Please enter the number of elements: ";
+	//std::cin >> arrLength;
+	int random_number; 
+
+	for (int i = 0; i < arrLength; i++) {
+		random_number = RandomInRange(1, 100);
+		arr[i] = random_number;
+	}
+}
+
+void AddArrayElement(int arr[100], int& arrLength, int number) {
+	arr[arrLength] = number;
+	arrLength++; 
+}
+
+void CopyPrimeNumbersToNewArray(int arr1[100], int arr2[100], int& arr1Length, int& arr2Length) {
+	for (int i = 0; i < arr1Length; i++) {
+		if (IsPrime(arr1[i])) {
+			AddArrayElement(arr2, arr2Length, arr1[i]);
+		}
+	}
+}
+
+void PrintArray(int A[100], int length) {
+	for (int i = 0; i < length; i++) {
+		std::cout << A[i] << " ";
+	}
+	std::cout << "\n";
+}
+
+int main() {
+	srand((unsigned)time(NULL));
+
+	int arr1[100], arr2[100], arr1Length = 0, arr2Length = 0;
+
+	arr1Length = ReadPositiveNumber("Please enter the number of elements: ");
+
+	FillArrayWithRandomNumbers(arr1, arr1Length);
+
+	std::cout << "\nArray1 elements:\n";
+	PrintArray(arr1, arr1Length);
+
+	CopyPrimeNumbersToNewArray(arr1, arr2, arr1Length, arr2Length);
+	std::cout << "\nArray2 elements:\n";
+	PrintArray(arr2, arr2Length);
+}
